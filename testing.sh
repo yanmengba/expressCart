d1eb8a0a4385bea841c4fdd816aff37ac7b72caf
Tim Mackey <xenserverarmy@users.noreply.github.com>
2019-11-05 10:45:58 -0500

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -6,6 +6,9 @@ then
 	tar -xvf /seeker/seeker-agent.tgz
 	cd /var/expressCart/
 	npm install /tmp/testing/package/*.tgz 
+	npm run demo -- --require ./node_modules/@synopsys-sig/seeker
+else
+	npm run demo
 fi
 
-npm run demo
+
