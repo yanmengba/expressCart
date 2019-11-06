62af3fbfa25a622dbe2085b3ce8b54f4976e9c81
Tim Mackey <xenserverarmy@users.noreply.github.com>
2019-11-06 10:43:55 -0500

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+set +x
+
 if [ -d "/seeker" ] 
 then
 	cd /tmp/testing
 	tar -xvf /seeker/seeker-agent.tgz
 	cd /var/expressCart/
+	echo "Install sensor"
 	npm install /tmp/testing/package/*.tgz 
+	env | grep SEEK
 	npm run testdata
 	node app.js --require ./node_modules/@synopsys-sig/seeker
 else
